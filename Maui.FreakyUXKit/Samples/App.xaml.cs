04720026abf3f7354e61acf624bd5f1afe03e0b6
Ali <31090457+FreakyAli@users.noreply.github.com>
2025-06-11 05:59:03 +0530

    fix: shell example

--- Maui.FreakyUXKit/Samples/App.xaml.cs
+++ Maui.FreakyUXKit/Samples/App.xaml.cs
@@ -5,10 +5,6 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-	}
-#nullable enable
-	protected override Window CreateWindow(IActivationState? _)
-	{
-		return new Window(new MainPage());
+		MainPage = new AppShell();
 	}
 }
