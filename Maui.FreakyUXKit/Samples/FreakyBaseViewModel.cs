b863a0a4634409ac6aaf6ae01ee5aef84322fd4b
Ali <hakimgulamali88@gmail.com>
2025-06-22 17:30:38 +0530

    fix: hire and follow button

--- Maui.FreakyUXKit/Samples/FreakyBaseViewModel.cs
+++ Maui.FreakyUXKit/Samples/FreakyBaseViewModel.cs
@@ -1,18 +1,37 @@
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Samples;
 
 public class FreakyBaseViewModel : ObservableObject
 {
     public ICommand BackButtonCommand { get; }
+    public ICommand HireCommand { get; }
+    public ICommand FollowCommand { get; }
 
     public FreakyBaseViewModel()
     {
-        BackButtonCommand = new Command(ExecuteOnBackButtonClicked);
+        BackButtonCommand = new AsyncRelayCommand(ExecuteOnBackButtonClicked);
+        HireCommand = new AsyncRelayCommand(ExecuteOnHireButtonClicked);
+        FollowCommand = new AsyncRelayCommand(ExecuteOnFollowButtonClicked);
     }
 
-    private async void ExecuteOnBackButtonClicked()
+    private async Task ExecuteOnHireButtonClicked()
+    {
+        var link = "https://www.linkedin.com/in/freakyali/";
+        Uri uri = new(link);
+        await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+    }
+
+    private async Task ExecuteOnFollowButtonClicked()
+    {
+        var link = "https://github.com/FreakyAli";
+        Uri uri = new(link);
+        await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+    }
+
+    private async Task ExecuteOnBackButtonClicked()
     {
         await Shell.Current.GoToAsync("..");
     }
